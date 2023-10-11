0686f1bcf79caca6890cb6e7176ff0085daffde1
Ankit0saw <145410097+Ankit0saw@users.noreply.github.com>
2023-10-11 14:41:06 +0530

    Create and update

---
+++ CPP files/printing 1 to 10 using recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class abc1
+{
+public:
+    void display()
+    {
+        disp(1);
+    }
+    void disp(int i)
+  {
+    if(i<=10)
+    {
+        cout<<i<<endl;
+        disp(i+1);
+    }
+  }
+};
+int main()
+{
+    abc1 ob;
+    ob.display();
+    getch();
+}

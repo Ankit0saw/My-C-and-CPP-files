3bf3eba9e49cd37ec7933739ff2547689b600fe2
Ankit0saw <145410097+Ankit0saw@users.noreply.github.com>
2023-11-10 08:37:18 +0530

    Update enter board percentage and get suggestions.cpp

--- CPP files/enter board percentage and get suggestions.cpp
+++ CPP files/enter board percentage and get suggestions.cpp
@@ -1,1 +1,43 @@
-
+#include<iostream>
+using namespace std;
+class abc1
+{
+    int marks;
+public:
+    void get()
+    {
+        cout<<"enter marks\n";
+        cin>>marks;
+        if(marks>=65)
+        {
+            if(marks>=85)
+            {
+                cout<<"eligible for science,commerce,arts";
+            }
+            else if(marks>=75)
+            {
+                cout<<"eligible for commeerce and arts";
+            }
+            else
+            {
+                cout<<"eligible for arts";
+            }
+        }
+        else
+        {
+            if(marks>=60)
+            {
+                cout<<"wait for 2nd round result";
+            }
+            else
+            {
+                cout<<"not eligible";
+            }
+        }
+    }
+};
+int main()
+{
+    abc1 ob;
+    ob.get();
+}

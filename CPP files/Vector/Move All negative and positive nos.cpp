6ac1f57059378d76cc525611ed67e0421858c022
Ankit0saw <145410097+Ankit0saw@users.noreply.github.com>
2023-12-31 00:21:31 +0530

    Create Move All negative and positive nos.cpp

---
+++ CPP files/Vector/Move All negative and positive nos.cpp
@@ -0,0 +1,33 @@
+// this code will move all the -ve nos to left side of array and hence +ve nos to the right side of array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  vector<int> nums{3,-1,5,-5,8,12,-58,63,4,21,-35,7,4,-4,6,5};
+  int pos,neg;
+  neg=0,pos=nums.size()-1;
+  
+  cout<<"before :"<<endl;
+  for(int i=0;i<nums.size();i++){
+    cout<<nums[i]<<"  ";
+  }
+  
+  while(neg<=pos){
+      if(nums[neg]<0){
+        neg++;
+      }
+      else{
+       swap(nums[neg],nums[pos]);
+       pos--;
+     }
+  } 
+  
+  cout<<"\nafter :"<<endl;
+  for(int i=0;i<nums.size();i++){
+    cout<<nums[i]<<"  ";
+  }
+  return 0;
+}

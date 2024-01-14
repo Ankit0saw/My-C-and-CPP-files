a063a8d3632356b4c8d3a709519df30bc414e18d
Ankit0saw <145410097+Ankit0saw@users.noreply.github.com>
2024-01-15 01:44:45 +0530

    Create Pivot smallest.cpp

---
+++ CPP files/Array/Pivot smallest.cpp
@@ -0,0 +1,50 @@
+//finding pivot which is smallest
+// given input must be sorted and rotated
+// i/p => 9,10,2,3,4,6,8
+// o/p => index: 2 & value: 2
+
+#include<iostream>
+using namespace std;
+
+int findPivot(int arr[],int n){
+
+    int s=0;
+    int e=n-1;
+    int mid= s+ (e-s)/2;
+
+    while(s<=e){
+        if(s==e){
+            return s;
+        }
+        if(mid<=e && arr[mid-1]>arr[mid]){
+            return mid;
+        }
+        if(mid-1 >= s && arr[mid+1]<arr[mid]){
+            return mid+1;
+        }
+
+        if(arr[s]>arr[mid]){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid= s+ (e-s)/2;
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={22,23,29,33,65,2,4,6,8,9,15,17,18};
+    int size=6;
+    int ans=findPivot(arr,size);
+
+    if(ans== -1){
+        cout<<"input mein error hein";
+    }
+    else{
+        cout<<"ans index "<<ans<<endl;
+        cout<<"ans value "<<arr[ans]<<endl;
+    }
+    return 0;
+}
